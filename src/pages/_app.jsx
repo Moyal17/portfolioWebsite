2dbc637f8065b5f2869f4bbc2c8660595fbc8512
Dor Moyal <moyal17@gmail.com>
2022-11-25 18:46:28 +0200

    add NProgress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,16 @@
 import { ThemeProvider } from 'next-themes';
+import Router from "next/router";
+import NProgress from 'nprogress';
 import AppProvider from '../context/AppContext';
 import '../index.scss';
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
+
+NProgress.configure({ easing: 'ease', speed: 400 });
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 function MyApp({ Component, pageProps }) {
   return (
